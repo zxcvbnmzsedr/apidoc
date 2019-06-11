7ee64d561680c7306199ba731db918e958d9e5ba
zxcvbnmzsedr <tianzneg@gmail.com>
2019-06-11 18:17:15 +0800

    好像出了大问题。。一个是编译时寻找一个是运行时寻找

---
+++ apidoc-maven-plugin/src/main/java/com/ztianzeng/apidoc/maven/DocMojo.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright © 2017 - 2020 Cnabc. All Rights Reserved.
+ */
+
+package com.ztianzeng.apidoc.maven;
+
+import com.ztianzeng.apidoc.Reader;
+import com.ztianzeng.apidoc.SourceBuilder;
+import com.ztianzeng.apidoc.models.OpenAPI;
+import com.ztianzeng.apidoc.models.info.Info;
+import com.ztianzeng.apidoc.utils.Json;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.util.Set;
+
+/**
+ * @goal apidoc
+ */
+@Mojo(name = "openapi")
+public class DocMojo extends AbstractMojo {
+
+    /**
+     * @parameter expression="${src}" default-value="src"
+     */
+    @Parameter(property = "src")
+    private String src;
+
+    @Parameter(property = "title", defaultValue = "doc")
+    private String title;
+
+    @Parameter(property = "version", defaultValue = "1.0")
+    private String version;
+
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("扫描地址 " + src);
+        execute(src);
+    }
+
+    public void execute(String url) {
+        SourceBuilder sourceBuilder = new SourceBuilder(url);
+
+        Reader reader = new Reader(new OpenAPI(), sourceBuilder);
+
+        Set<Class<?>> controllerData = null;
+        try {
+            controllerData = sourceBuilder.getControllerData();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        OpenAPI openAPI = reader.read(controllerData);
+        Info info = new Info();
+        info.title(title);
+        info.setVersion(version);
+
+        openAPI.setInfo(info);
+
+        Json.prettyPrint(openAPI);
+    }
+
+
+}
