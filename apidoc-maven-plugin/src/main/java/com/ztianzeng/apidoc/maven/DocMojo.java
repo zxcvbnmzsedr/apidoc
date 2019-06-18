554a78f33e9f558569c549e88d3bb89a02492a7d
zxcvbnmzsedr <tianzneg@gmail.com>
2019-06-18 22:05:06 +0800

    通过重新加载classloader完成plugin

--- apidoc-maven-plugin/src/main/java/com/ztianzeng/apidoc/maven/DocMojo.java
+++ apidoc-maven-plugin/src/main/java/com/ztianzeng/apidoc/maven/DocMojo.java
@@ -10,20 +10,31 @@ import com.ztianzeng.apidoc.SourceBuilder;
 import com.ztianzeng.apidoc.models.OpenAPI;
 import com.ztianzeng.apidoc.models.info.Info;
 import com.ztianzeng.apidoc.utils.Json;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * @goal apidoc
+ * @requiresDependencyResolution runtime
  */
-@Mojo(name = "openapi")
+@Mojo(name = "openapi", defaultPhase = LifecyclePhase.COMPILE,
+        requiresDependencyResolution = ResolutionScope.COMPILE)
 public class DocMojo extends AbstractMojo {
-
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject mavenProject;
     /**
      * @parameter expression="${src}" default-value="src"
      */
@@ -37,18 +48,38 @@ public class DocMojo extends AbstractMojo {
     private String version;
 
 
+
+
     @Override
     public void execute() {
+        try {
+            // 加载classloader
+            Set<URL> urls = new HashSet<>();
+            List<String> elements = mavenProject.getRuntimeClasspathElements();
+            for (String element : elements) {
+                urls.add(new File(element).toURI().toURL());
+            }
+
+            ClassLoader contextClassLoader = URLClassLoader.newInstance(
+                    urls.toArray(new URL[0]),
+                    Thread.currentThread().getContextClassLoader());
+
+            Thread.currentThread().setContextClassLoader(contextClassLoader);
+
+        } catch (DependencyResolutionRequiredException | MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
         getLog().info("扫描地址 " + src);
         execute(src);
     }
 
     public void execute(String url) {
+
         SourceBuilder sourceBuilder = new SourceBuilder(url);
 
         Reader reader = new Reader(new OpenAPI(), sourceBuilder);
 
-        Set<JavaClass> controllerData = null;
+        Set<JavaClass> controllerData;
         controllerData = sourceBuilder.getControllerData();
 
 
