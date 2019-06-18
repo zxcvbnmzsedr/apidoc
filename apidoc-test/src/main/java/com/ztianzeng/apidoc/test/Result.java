8f101ccb65c68dd3c1e01fabf289a3a29f8a1125
zxcvbnmzsedr <tianzneg@gmail.com>
2019-06-18 20:48:53 +0800

    maven plugin 当前项目OK

---
+++ apidoc-test/src/main/java/com/ztianzeng/apidoc/test/Result.java
@@ -0,0 +1,23 @@
+package com.ztianzeng.apidoc.test;
+
+import lombok.Getter;
+
+/**
+ * @author zhaotianzeng
+ * @version V1.0
+ * @date 2019-06-11 13:13
+ */
+@Getter
+public class Result<T> {
+    private String msg;
+    private T data;
+
+    protected Result() {
+    }
+
+    private Result(String msg, T t) {
+        this.msg = msg;
+        this.data = t;
+    }
+
+}
---
+++ apidoc-test/src/main/java/com/ztianzeng/apidoc/test/TestController.java
@@ -0,0 +1,56 @@
+package com.ztianzeng.apidoc.test;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 测试控制器
+ *
+ * @author zhaotianzeng
+ * @version V1.0
+ * @date 2019-05-27 13:22
+ */
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+
+    /**
+     * 新增一个实例
+     *
+     * @param createParam 创建对象
+     * @return 创建后的信息
+     */
+    @PostMapping(value = "/create")
+    public CreateVO add(@RequestBody @Valid CreateParam createParam) {
+        return new CreateVO();
+    }
+
+    /**
+     * 新增一个实例2
+     *
+     * @param createParam2 创建对象2
+     */
+    @PostMapping(value = "/create2")
+    public List<CreateVO> create2(@Valid @RequestBody List<CreateParam> createParam2) {
+        return new LinkedList<>();
+    }
+
+    /**
+     * 获取一个实例
+     *
+     * @param userId 用户ID
+     * @param sex    性别
+     * @return 返回信息
+     */
+    @GetMapping(value = "/get")
+    public Result<CreateVO> get(@RequestParam(value = "userId", required = false) String userId,
+                                @RequestParam(value = "sex2") String sex) {
+        return new Result<>();
+    }
+
+
+}
